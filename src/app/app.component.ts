1b2ace72196aed3652a36c4af4281e16b3e2c4b7
Hafeez4441 <khawajahafeez@gmail.com>
2023-07-25 15:31:31 +0500

    Hafeez_Angular_07/25/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'devops-hint-hafeez';
+  title = 'devops-hint-hafeez 1';
 }
